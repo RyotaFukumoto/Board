5addeb2ff95343e6c9caf8da5a93939a17a661a9
Hayashi <k014c1105@it-neec.jp>
2016-06-06 11:04:49 +0900

    add 2files

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+  <title>ログインページ</title>
+</head>
+<body>
+    <form action="http://localhost/Board/board.php" method="get">
+      <input type="text" name="name" value="">
+      <input type="submit" name="login" value="login">
+    </form>
+</body>
+</html>
