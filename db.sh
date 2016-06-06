d12444b87266de6876f1e1000d09922666bbe793
mochizuki <k014c1121@it-neec.jp>
2016-06-06 12:03:18 +0900

    add

--- db.sh
+++ db.sh
@@ -2,7 +2,9 @@
 cd /opt/lampp/bin
 ./mysql -uroot -e "drop database BoardDB;"
 ./mysql -uroot -e  "create database BoardDB char set utf8;"
-./mysql -uroot BoardDB -e "create table User(name varchar(255) not null primary key ,password varchar(255));"
-./mysql -uroot BoardDB -e "create table Board(name varchar(255) not null primary key,time datetime,text varchar(255));"
-# ./mysql -uroot todoDB -e "insert into ToDoList(text) value('testdata');"
-# ./mysql -uroot todoDB -e "select * from ToDoList;"
+./mysql -uroot BoardDB -e "create table User(name varchar(255) not null primary key ,password varchar(255) );"
+./mysql -uroot BoardDB -e "create table Board(id int not null auto_increment primary key,name varchar(255)  ,time datetime,text varchar(255));"
+./mysql -uroot BoardDB -e "insert into User(name,password) value('yamada','taro');"
+./mysql -uroot BoardDB -e "select * from User;"
+./mysql -uroot BoardDB -e "insert into Board(name,time,text) value('yamada','YYYYMMDD[ hh:mm:ss[.mmm]]','taro');"
+./mysql -uroot BoardDB -e "select * from Board;"
