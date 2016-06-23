a45d6d8d22af1e33a188b2f15d9c52178f011d53
Hayashi <k014c1105@it-neec.jp>
2016-06-24 00:34:27 +0900

    bag fixed

--- message.php
+++ message.php
@@ -6,21 +6,28 @@
     $dbh = new PDO($dsn, $user, $password);
     $dbh->query('SET NAMES utf8');
     $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    $name  = $_COOKIE['name'];
   }catch(PDOException $e){
     die('エラー');
   }
-  $sql = 'SELECT * FROM Board ORDER BY id DESC;';
-  $stmt = $dbh->prepare($sql);
-  $stmt->execute();
-  while($task = $stmt->fetch(PDO::FETCH_ASSOC)){
-    echo "<div class='media message' style='word-wrap: break-word;'>";
-    echo "<form method='get' action=''>".$task['name']."  ";
-    echo $task['time'];
-    echo '<input type="hidden" name="name" value='.$name.' >';
-    echo "　<button type='submit' name='delete' style='float: right;' class='btn btn-default btn-xs' value='".$task['id']."'><i class='fa fa-trash-o' aria-hidden='true'></i></button>";
-    echo "<br>".$task['text'];
-    echo "</form>";
-    echo "</div>";
+  if(isset($_COOKIE['name'])){
+    $name  = $_COOKIE['name'];
+    $sql = 'SELECT * FROM Board ORDER BY id DESC;';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    while($task = $stmt->fetch(PDO::FETCH_ASSOC)){
+      echo "<div class='media message' style='word-wrap: break-word;'>";
+        echo "<form method='get' class='media-body' action=''>";
+          echo $task['name']."  ".$task['time'];
+          echo '<input type="hidden" name="name" value='.$name.' >';
+          echo "　<button type='submit' name='delete' style='float: right;' class='btn btn-default btn-xs' value='".$task['id']."'><i class='fa fa-trash-o' aria-hidden='true'></i></button>";
+        echo "</form>";
+        echo $task['text'];
+      echo "</div>";
+    }
+  }else{
+    echo "<script type='text/javascript'>";
+    echo "alert('ログインし直してください。');";
+    echo "location.href = 'index.php';";
+    echo "</script>";
   }
 ?>
